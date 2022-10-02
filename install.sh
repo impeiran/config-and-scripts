e6b8126fd00f9665ef246e17c85ec3e9ad4137ff
impeiran <1070045244@qq.com>
2022-10-02 19:40:24 +0800

    feat: update

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
+mkdir -p ~/business
+mkdir -p ~/repo_personal
+mkdir -p ~/repo_fork
+
 bash ./terminal/install.sh
--- terminal/install.sh
+++ terminal/install.sh
@@ -6,6 +6,10 @@ brew install --cask iterm2
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# zsh color presets
+# set color presets MaterialDarker
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git ~/repo_personal/iTerm2-Color-Schemes
+
 # zsh theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
